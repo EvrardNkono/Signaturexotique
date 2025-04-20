72be5c504ebd03f127d0480201fd3a9feb278460
EvrardNkono <evrardnkono870@gmail.com.com>
2025-04-20 23:11:54 +0100

    Ajout de la gestion des variables d'environnement et modifications des composants

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,2 @@
 // src/config.js
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
-export default BASE_URL;
-// src/config.js
-export const API_URL = process.env.REACT_APP_API_URL;
+export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
